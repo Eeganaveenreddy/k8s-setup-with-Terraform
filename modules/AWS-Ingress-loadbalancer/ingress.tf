c64cd1461f4f42fa79473a872344c9fd1d515c60
naveen <enreddy619@gmail.com>
2025-03-03 20:07:05 +0800

    ingress loadbalncer

---
+++ modules/AWS-Ingress-loadbalancer/ingress.tf
@@ -0,0 +1,33 @@
+resource "kubernetes_ingress_v1" "app_ingress" {
+  metadata {
+    name      = "app-ingress"
+    namespace = "default"
+    annotations = {
+      "kubernetes.io/ingress.class" = "alb"
+      "alb.ingress.kubernetes.io/scheme" = "internet-facing"  # Change to "internal" if needed
+      "alb.ingress.kubernetes.io/target-type" = "ip"
+    }
+  }
+
+  spec {
+    rule {
+      host = "cloudops-platforms.cloud"  # Replace with your domain
+
+      http {
+        path {
+          path      = "/*"
+          path_type = "Prefix"
+
+          backend {
+            service {
+              name = kubernetes_service.app_service.metadata[0].name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ modules/AWS-Ingress-loadbalancer/service.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_service" "app_service" {
+  metadata {
+    name      = "app-service"
+    namespace = "default"
+  }
+
+  spec {
+    selector = {
+      app = "my-app"  # This must match your deployment's labels
+    }
+
+    port {
+      protocol    = "TCP"
+      port        = 80
+      target_port = 8080  # Port on your pod
+    }
+
+    type = "NodePort"  # Can also be "ClusterIP" if using an ALB
+  }
+}
