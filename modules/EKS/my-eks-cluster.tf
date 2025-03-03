7af49100b77b94f3f39f639904669a3c835bdefa
naveen <enreddy619@gmail.com>
2025-03-04 00:46:58 +0800

    added update-kubeconfig command in terraform code.

--- modules/EKS/my-eks-cluster.tf
+++ modules/EKS/my-eks-cluster.tf
@@ -11,6 +11,15 @@ resource "aws_eks_cluster" "eks_cluster" {
     depends_on = [ var.eks_policy_attach ]
 }
 
+resource "null_resource" "update_kubeconfig" {
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --region ${var.region} --name ${aws_eks_cluster.eks_cluster.name}"
+  }
+  depends_on = [ aws_eks_cluster.eks_cluster ]
+  
+}
+
+
 output "eks_cluster_name_output" {
   value = aws_eks_cluster.eks_cluster.name
 }
