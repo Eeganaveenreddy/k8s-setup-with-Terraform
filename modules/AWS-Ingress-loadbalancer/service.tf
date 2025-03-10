6d346e7f6b8d5a9af7ef5b98ccd998e5bd19c120
naveen <enreddy619@gmail.com>
2025-03-10 15:01:28 +0800

    changed nginx port 8080 to 80

--- modules/AWS-Ingress-loadbalancer/service.tf
+++ modules/AWS-Ingress-loadbalancer/service.tf
@@ -2,15 +2,6 @@ resource "kubernetes_service" "app_service" {
   metadata {
     name      = "app-service"
     namespace = "default"
-    # annotations = {
-    #   "service.beta.kubernetes.io/aws-load-balancer-type" = "nlb"       # Specify NLB type
-    #   "service.beta.kubernetes.io/aws-load-balancer-scheme" = "internet-facing"  # Change to "internal" if needed
-    #   "service.beta.kubernetes.io/aws-load-balancer-healthcheck-protocol" = "TCP"
-    #   "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
-    #   "service.beta.kubernetes.io/aws-load-balancer-healthcheck-port"     = "traffic-port"
-    #   "service.beta.kubernetes.io/aws-load-balancer-name" = "terr-nlb"
-    #   "service.beta.kubernetes.io/aws-load-balancer-security-groups" = aws_security_group.nlb_sg.id  # Attach NLB Security Group
-    # }
   }
 
   spec {
@@ -21,9 +12,9 @@ resource "kubernetes_service" "app_service" {
     port {
       protocol    = "TCP"
       port        = 80
-      target_port = 8080  # Port on your pod
+      target_port = 80  # Port on your pod
     }
 
-    type = "LoadBalancer"  # Can also be "ClusterIP" if using an ALB
+    type = "ClusterIP"
   }
 }
